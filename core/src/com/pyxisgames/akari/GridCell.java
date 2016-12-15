332ca855d08ae44ece54ba945333902806a7d0f0
Shannon Lui <shannonlui@hotmail.com>
2016-12-15 10:12:25 -0500

    add light bulb to cells

--- core/src/com/pyxisgames/akari/GridCell.java
+++ core/src/com/pyxisgames/akari/GridCell.java
@@ -1,18 +1,72 @@
 package com.pyxisgames.akari;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 /**
+ * if black: count = number on the cell
+ * else: count = number of light bulbs lighting this cell
  * Created by shannonlui on 2016-12-13.
  */
 
 public class GridCell extends Sprite {
-    private Vector2 coordinates;
+    private Vector2 coords;
+    private boolean isBlack = false;
+    private boolean hasBulb = false;
+    private int count = 0;
 
     public GridCell(Texture texture, int x, int y) {
         super(texture);
-        coordinates = new Vector2(x, y);
+        coords = new Vector2(x, y);
+    }
+
+    public GridCell(Texture texture, int x, int y, boolean isBlack) {
+        this(texture, x, y);
+        this.isBlack = isBlack;
+        if (isBlack) {
+            setColor(Color.BLACK);
+        }
+    }
+
+    public Vector2 getCoords() {
+        return coords;
+    }
+
+    public void tintBlack() {
+        isBlack = true;
+        setColor(Color.DARK_GRAY);
+    }
+
+    public void lightUp() {
+        setColor(Color.valueOf("#f4e648"));
+
+    }
+
+    public void addBulb() {
+        hasBulb = true;
+        count++;
+        lightUp();
+    }
+
+    public void removeBulb() {
+        hasBulb = false;
+        decCount();
+    }
+
+    public void decCount() {
+        count--;
+        if (count == 0) {
+            setColor(Color.WHITE);
+        }
+    }
+
+    public boolean isBlack() {
+        return isBlack;
+    }
+
+    public boolean hasBulb() {
+        return hasBulb;
     }
 }
--- core/src/com/pyxisgames/akari/screens/PlayScreen.java
+++ core/src/com/pyxisgames/akari/screens/PlayScreen.java
@@ -2,16 +2,22 @@ package com.pyxisgames.akari.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.pyxisgames.akari.Akari;
 import com.pyxisgames.akari.GridCell;
+
+import java.util.Iterator;
 
 /**
  * Created by shannonlui on 2016-12-11.
@@ -19,58 +25,144 @@ import com.pyxisgames.akari.GridCell;
 
 public class PlayScreen implements Screen {
     private Akari game;
-    Texture img;
     private OrthographicCamera cam;
     private Viewport vp;
-    private Stage grid;
-    private Sprite s;
-    private Array<Sprite> cells;
+    private ObjectMap<Vector2, GridCell> cellMap = new ObjectMap();
+
+    // Grid dimensions
     private int gridWidth = 7;
     private int gridHeight = 7;
-    private float borderWidth = 8;
+    private float borderWidth = 5;
+
+    // Textures and Sprites
+    private Texture cellTexture;
+    private Texture lightBulb;
+    private Sprite bulbSprite;
 
     public PlayScreen(Akari game) {
         this.game = game;
-        img = new Texture("badlogic.jpg");
         cam = new OrthographicCamera();
         cam.position.set(Akari.GAME_WIDTH / 2, Akari.GAME_HEIGHT / 2, 0);
         vp = new FitViewport(Akari.GAME_WIDTH, Akari.GAME_HEIGHT, cam);
-        s = new GridCell(img, 0 , 0);
-        s.setSize(40, 40);
-        s.setPosition(40,40);
+
+        // Set up textures and sprites
+        cellTexture = new Texture("cell.png");
+        cellTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        lightBulb = new Texture("light_bulb9.png");
+        lightBulb.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        bulbSprite = new Sprite(lightBulb);
+        bulbSprite.setColor(Color.DARK_GRAY);
+
         createGrid();
     }
 
     public void createGrid() {
         float cellLength = (Akari.GAME_WIDTH - ((gridWidth + 1) * borderWidth))/gridWidth;
-        float gridYPosition = (Akari.GAME_HEIGHT - ((gridHeight - 1) * borderWidth) - cellLength * gridHeight) / 2;
-        System.out.println(cellLength);
+        float yStartPos = (Akari.GAME_HEIGHT - ((gridHeight - 1) * borderWidth) - cellLength * gridHeight) / 2;
         float x = borderWidth;
-        float y = gridYPosition;
+        float y = yStartPos;
 
-        cells = new Array<Sprite>();
+        // Adjust the size of the light bulb sprite
+        float bulbHeight = cellLength * 3/4;
+        bulbSprite.setSize(bulbHeight/bulbSprite.getHeight() * bulbSprite.getWidth(), bulbHeight);
+
+        // Add cells to the cellMap
         for (int i = 0; i < gridWidth; i++) {
             for (int j = 0; j < gridHeight; j++) {
-                Sprite cell = new GridCell(img, i, j);
+                GridCell cell = new GridCell(cellTexture, i, j);
+                if ((i == 2 && j == 4) || (i==4 && j ==1)) {
+                    cell.tintBlack();
+                }
                 cell.setSize(cellLength, cellLength);
                 cell.setPosition(x, y);
-                cells.add(cell);
+
+                cellMap.put(new Vector2(i, j), cell);
                 y += borderWidth + cellLength;
             }
             x += borderWidth + cellLength;
-            y = gridYPosition;
+            y = yStartPos;
+        }
+    }
+
+    public void update(float delta) {
+        // If touch detected, get touch position
+        if (Gdx.input.justTouched()) {
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            cam.unproject(touchPos);
+
+            // Iterate through the grid to check if a cell has been touched.
+            Iterator<GridCell> itr = cellMap.values();
+            GridCell cell;
+            while (itr.hasNext()) {
+                cell = itr.next();
+                if (!cell.isBlack() && cell.getBoundingRectangle().contains(touchPos.x, touchPos.y)) {
+                    System.out.println("Touched " + cell.getCoords());
+                    if (cell.hasBulb()) {
+                        cell.removeBulb();
+                    } else {
+                        cell.addBulb();
+                        lightNeighbours(cell.getCoords());
+                    }
+
+                }
+            }
+        }
+    }
+
+    // Light up the cells in the same column or row as the given vector
+    public void lightNeighbours(Vector2 coords) {
+        lightRow(coords, -1);
+        lightRow(coords, 1);
+        lightColumn(coords, -1);
+        lightColumn(coords, 1);
+    }
+
+    // Light up cells in this row up until a black cell or the end of the grid
+    public void lightRow(Vector2 coords, int value) {
+        float currX = coords.x + value;
+        float y = coords.y;
+        GridCell curr = cellMap.get(new Vector2(currX, y));
+        while (curr != null && !curr.isBlack()) {
+            curr.lightUp();
+            currX = currX + value;
+            curr = cellMap.get(new Vector2(currX, y));
+        }
+    }
+
+    // Light up cells in this column up until a black cell or the end of the grid
+    public void lightColumn(Vector2 coords, int value) {
+        float currY = coords.y + value;
+        float x = coords.x;
+        GridCell curr = cellMap.get(new Vector2(x, currY));
+        while (curr != null && !curr.isBlack()) {
+            curr.lightUp();
+            currY = currY + value;
+            curr = cellMap.get(new Vector2(x, currY));
         }
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        update(delta);
+
+        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.setProjectionMatrix(cam.combined);
+
         game.batch.begin();
-        //s.draw(game.batch);
-        for (Sprite sprite : cells) {
-            sprite.draw(game.batch);
+
+        // Draw the grid
+        Iterator<GridCell> itr = cellMap.values();
+        GridCell cell;
+        while (itr.hasNext()) {
+            cell = itr.next();
+            cell.draw(game.batch);
+            // Draw light bulb on top of cell if cell has bulb
+            if (cell.hasBulb()) {
+                bulbSprite.setCenter(cell.getX() + cell.getWidth()/2, cell.getY() + cell.getHeight()/2);
+                bulbSprite.draw(game.batch);
+            }
         }
         game.batch.end();
     }
@@ -86,22 +178,20 @@ public class PlayScreen implements Screen {
 
     @Override
     public void pause() {
-
     }
 
     @Override
     public void resume() {
-
     }
 
     @Override
     public void hide() {
-
     }
 
     @Override
     public void dispose() {
         game.batch.dispose();
-        img.dispose();
+        cellTexture.dispose();
+        lightBulb.dispose();
     }
 }
